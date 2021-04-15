009ea23e3bc48c0b4320f55754e3705d5de34abf
nikita <nikita@DESKTOP-EEKA4M8.localdomain>
2021-04-15 15:11:13 +0300

    fix

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def configure():
     config['video_fetch'] = sys.argv[2] == "yes"
     config['page_url'] = sys.argv[1]
     config['is_single'] = config['page_url'].find('/p/') != -1
+    config['is_count'] = len(sys.argv) >= 6
+    if config['is_count']:
+        config['count'] = int(sys.argv[5])
     print(config)
     return config
 
@@ -27,31 +30,45 @@ config = configure()
 def find_links(driver, links):
     links_els = driver.find_elements_by_xpath("//a[starts-with(@href, '/p/')]")
     for elem in links_els:
-        if(len(sys.argv) >= 6 and len(links) >= int(sys.argv[5])):
+        if(config['is_count'] and len(links) >= config['count']):
+            print('STOPPED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
             return False
         push_link(links, elem.get_attribute("href"))
         print(elem.get_attribute("href"))      
     return True
 
-def infinite_scroll(driver, links):
+def infinite_scroll(driver, links, total_expecting):
     SCROLL_PAUSE_TIME = 1.25
-    last_height = driver.execute_script("return document.body.scrollHeight")
+    #last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(SCROLL_PAUSE_TIME)
-        if(not(find_links(driver, links))):
-            return
+        t = len(links)
+        i = 0
+        while len(links) == t:
+            print("WHILE!!!!!!!!!!!!!!!")
+            if i == 8:
+                return
+                
+            if(not(find_links(driver, links))):
+                return
+            i+=1
+            
+            time.sleep(SCROLL_PAUSE_TIME)    
        
         # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
+        #time.sleep(SCROLL_PAUSE_TIME)
 
         # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
+        #new_height = driver.execute_script("return document.body.scrollHeight")
+        #if new_height == last_height:
+        #    break
+        #last_height = new_height
+        print(len(links))
+        
+
 
 def login(driver):
     driver.get('https://www.instagram.com')
@@ -74,6 +91,7 @@ def login(driver):
     login_button_ = driver.find_element_by_xpath(
         "//form[@class='HmktE']/div/div[3]/button")
     login_button_.click()
+    time.sleep(5)
 
 async def download_file(_file, ext):
       
@@ -88,11 +106,12 @@ async def download_file(_file, ext):
 
 async def proceed_link(page_link, driver):
     driver.get(page_link)
+    
     images_links = set()
     videos_links = set()
     wait = WebDriverWait(driver, 10)
     
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".ltEKP img.FFVAD, .tWeCl")))
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".ltEKP img.FFVAD, ._5wCQW video.tWeCl")))
 
     try:
         right = driver.find_element_by_css_selector(".coreSpriteRightChevron")
@@ -105,7 +124,7 @@ async def proceed_link(page_link, driver):
         imgs = driver.find_elements_by_css_selector(".ltEKP img.FFVAD")
         print(imgs)
         if config['video_fetch']:
-            videos = driver.find_elements_by_css_selector(".tWeCl")
+            videos = driver.find_elements_by_css_selector("._5wCQW video.tWeCl")
             previews = driver.find_elements_by_css_selector("._8jZFn")
           
             for preview in previews:
@@ -127,38 +146,42 @@ async def proceed_link(page_link, driver):
     await asyncio.gather(*[download_file(_file, '.mp4') for _file in videos_links])
    
 def push_link(links, link):
-    if(len(sys.argv)>=6):
+    if(config['is_count']):
         links.append(link)
         links = list(dict.fromkeys(links))
     else :
         links.add(link)
    
 def init_links():
-    if(len(sys.argv)>=6):
+    if(config['is_count']):
+        print("LIST!!!!!!!!!!!!!!!!!!!!!!!!")
         return list()
     else :
+        print("SET!!!!!!!!!!!!!!!!!!!!!!!!")
         return set()
 
 async def main():
     driver = webdriver.Chrome()
-    login(driver)
-    time.sleep(5)
+    
+    
     if config['is_single']:
 
-        if config['video_fetch']:
-            driver.delete_all_cookies()
+        if not(config['video_fetch']):
+            login(driver)
 
         await proceed_link(config['page_url'], driver)
 
     else :
-        
+        login(driver)
         driver.get(config['page_url'])
         total_expecting = driver.find_element_by_css_selector('.g47SY').text
         links = init_links()
-        infinite_scroll(driver, links)
-       
+        
         if config['video_fetch']:
-            driver.delete_all_cookies()
+           driver.delete_all_cookies()
+        infinite_scroll(driver, links, int(total_expecting))
+       
+        
         
         print(str(len(links))+"/"+total_expecting)
 
